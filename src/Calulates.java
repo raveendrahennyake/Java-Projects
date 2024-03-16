32a1224351d36664751bc69637fbae23f8d96fea
raveendrahennyake <raveendralakshan5@gmail.com>
2024-03-16 22:27:40 +0530

    git commit _m "message2"

--- src/Calulates.java
+++ src/Calulates.java
@@ -2,73 +2,68 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
  class Calculate {
+     public void marks() {
+         Scanner x = new Scanner(System.in);
+         System.out.println("Choice your Operator ");
+         System.out.println("1. Addition");
+         System.out.println("2. Subtraction");
+         System.out.println("3. Multiplication");
+         System.out.println("4. Division");
+         System.out.print("Enter an option: ");
+         String choice = x.nextLine();
+         System.out.println("Enter your first number ");
+         float firstnumber = x.nextFloat();
 
-    public static void main(String[] args) {
+         System.out.println("Enter your Second number");
+         float secondnumber = x.nextFloat();
 
-    }
-    public void marks() {
-        Scanner x = new Scanner(System.in);
-        System.out.println("Choice your Operator ");
-        System.out.println("1. Addition");
-        System.out.println("2. Subtraction");
-        System.out.println("3. Multiplication");
-        System.out.println("4. Division");
-        System.out.print("Enter an option: ");
-        String choice = x.nextLine();
-        System.out.println("Enter your first number ");
-        float firstnumber = x.nextFloat();
+         float total = 0;
 
-        System.out.println("Enter your Second number");
-        float secondnumber = x.nextFloat();
+         switch (choice) {
+             case "1":
+                 total = firstnumber + secondnumber;
+                 System.out.println("Result: " + total);
+                 break;
+             case "2":
+                 total = firstnumber - secondnumber;
+                 System.out.println("Result: " + total);
+                 break;
+             case "3":
+                 total = firstnumber * secondnumber;
+                 System.out.println("Result: " + total);
+                 break;
+             case "4":
+                 if (secondnumber != 0) {
+                     total = firstnumber / secondnumber;
+                     System.out.println("Result: " + total);
+                 } else {
+                     System.out.println("Error: Division by zero");
+                 }
+                 break;
+             default:
+                 System.out.println("Invalid option. Try again.");
+                 break;
+         }
+     }
 
-        float total = 0;
 
-        switch (choice) {
-            case "1":
-                total = firstnumber + secondnumber;
-                System.out.println("Result: " + total);
-                break;
-            case "2":
-                total = firstnumber - secondnumber;
-                System.out.println("Result: " + total);
-                break;
-            case "3":
-                total = firstnumber * secondnumber;
-                System.out.println("Result: " + total);
-                break;
-            case "4":
-                if (secondnumber != 0) {
-                    total = firstnumber / secondnumber;
-                    System.out.println("Result: " + total);
-                } else {
-                    System.out.println("Error: Division by zero");
-                }
-                break;
-            default:
-                System.out.println("Invalid option. Try again.");
-                break;
-        }
-    }
-}
+     //Find Sum and Average of Array Elements
+     static int sum = 0;
+     static int Average = 1;
 
-//Find Sum and Average of Array Elements
-static int sum=0;
-static int Average=1;
-    public void findsum () {
-        int x []={1,2,3,4,5,6,7,8,};
-        for (int i=0;i<x.length;i+=1){
-            sum=sum+x[i];
+     public void findsum() {
+         int x[] = {1, 2, 3, 4, 5, 6, 7, 8,};
+         for (int i = 0; i < x.length; i += 1) {
+             sum = sum + x[i];
 
-            Average=sum/x.length;
+             Average = sum / x.length;
 
-        }
-        System.out.println("Sum is "+sum);
-        System.out.println("Avarage is "+Average);
-    }
-    public static void main(String[] args) {
-    }
+         }
+         System.out.println("Sum is " + sum);
+         System.out.println("Avarage is " + Average);
+     }
 
-//Find Prime Numbers in Java
+     //Find Prime Numbers in Java
      public void prime_number() {
          Scanner y = new Scanner(System.in);
          System.out.println("Enter your number");
@@ -90,55 +85,53 @@ static int Average=1;
              }
          }
      }
- 
- //Reverse a String in Java
-    void  Reverse () {
 
-        Scanner scanner = new Scanner(System.in);
+     //Reverse a String in Java
+     void Reverse() {
 
-        System.out.print("Enter a string: ");
-        String originalName = scanner.nextLine();
-        String reversedName = "";
+         Scanner scanner = new Scanner(System.in);
 
-        for (int i = originalName.length() - 1; i >= 0; i--) {
-            reversedName += originalName.charAt(i);
-        }
-        System.out.println("Reversed string is " + reversedName);
-    }
-    public static void main(String[] args) {
-    }
-     void _rev_number () {
-         Scanner x=new Scanner(System.in);
+         System.out.print("Enter a string: ");
+         String originalName = scanner.nextLine();
+         String reversedName = "";
+
+         for (int i = originalName.length() - 1; i >= 0; i--) {
+             reversedName += originalName.charAt(i);
+         }
+         System.out.println("Reversed string is " + reversedName);
+     }
+
+     void _rev_number() {
+         Scanner x = new Scanner(System.in);
          System.out.println("Enter your number");
-         int _num=x.nextInt();
-         Integer Originalname=_num;
-         int i=0;
-         }     
-//Reverses a number
-    static Scanner x=new Scanner(System.in);
+         int _num = x.nextInt();
+         Integer Originalname = _num;
+         int i = 0;
+     }
+
+     //Reverses a number
+     static Scanner x = new Scanner(System.in);
+
      void intReverise() {
 
-             System.out.println("Enter your number");
-             int _Reve_num=x.nextInt();
+         System.out.println("Enter your number");
+         int _Reve_num = x.nextInt();
 
-             int Answer=0;
+         int Answer = 0;
 
-             while (_Reve_num>0) {
+         while (_Reve_num > 0) {
 
-                 int Lastnum=_Reve_num% 10 ;
+             int Lastnum = _Reve_num % 10;
 
-                 Answer=Answer*10+Lastnum;
-                 _Reve_num /= 10;
-
-             }
-             System.out.println("answer is "+Answer);
+             Answer = Answer * 10 + Lastnum;
+             _Reve_num /= 10;
 
          }
-    public static void main(String[] args) {
-
-    }
+         System.out.println("answer is " + Answer);
 
      }
+
+ }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Calculate obj1=new Calculate();
+        Calculate obj1=new Calculate ();
         obj1.marks();
 
-         Calculate obj2=new Calculate ();
-         obj2.findsum();
+        Calculate obj2=new Calculate ();
+        obj1.findsum();
 
         Calculate obj3=new Calculate ();
         obj3.prime_number();
 
-        Calculate obj4=new Calculate();
-        obj4.Reverse();
+        Calculate obj4=new Calculate ();
+        obj4._rev_number();
 
-        Calculate obj5=new Calculate();
+        Calculate obj5=new Calculate ();
         obj5.intReverise();
+
+
 
 
 
